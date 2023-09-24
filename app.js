d7e33ca523cdaa0685d3621f6f3db34e171f1ce3
maheshbasnet089 <basnetmanish089@gmail.com>
2023-09-24 22:19:20 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const a = 10;
+var b = 20;
+b = 40;
+const name = "Manish"
+const sum = a + b 
+// console.log(sum)
+
+// array
+const numbers = [10,20,30] 
+// console.log(numbers[1])
+// to calculate length of array 
+// console.log(numbers.length)
+
+// objects
+const person = {
+    name : "Manish",
+    age : 21
+}
+// {key:value}
+console.log("This is person Name " +  person.name)
+console.log(person.age)
+
+
+// console.log("Welcome to NodeJs")
