481cb0fde22b2fa20ded690642bf7921eb59e287
maheshbasnet089 <basnetmanish089@gmail.com>
2023-09-26 22:20:51 +0545

    learn about json,nodemon,postman

--- app.js
+++ app.js
@@ -10,19 +10,29 @@ app.get("/",(req,res)=>{
   
     // res.send("<mark>Hello i m from home page</mark>")
     res.json({
-        manish : "I am from home page",
-        
+        message : "hello world ",
+        status : "student"
     })
 
 })
 
 app.get("/contact",(req,res)=>{
-    res.send("I am from contact page")
+    res.json({
+        message : "You are in contact Page"
+    })
 })
 
 app.get("/about",(req,res)=>{
-    res.send("I am from about page")
+    res.json({
+        status  : 200,
+        message : "You are in about page",
+        
+    })
 })
+
+
+
+
 
 // 2000 number ko room chae use gar yo project run garna lai 
 app.listen(2000,(req,res)=>{
